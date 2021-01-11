faa74951b171c2dedb3515049172bd48c6848f42
adomhamza <liveghost93@gmail.com>
2021-01-11 00:12:05 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'My first app',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey[200],
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.red[900],
-        ),
-        body: Center(
-          child: Text(
-            "Hello",
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w600,
-                color: Colors.black87),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My first app',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueGrey[200],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.red[800],
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/car3.jpg'),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[800],
+      ),
+    );
+  }
+}
